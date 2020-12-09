b18a24c4ac34bc26b9f6b0638cfd217467036bbb
ali alsir <75730589+alialsir@users.noreply.github.com>
2020-12-09 14:04:59 +0200

    Add files via upload

---
+++ hellow world.py
@@ -0,0 +1,6 @@
+# This is my first program in python .
+# It's simple hello world program (Print's Hello World) :-) .
+
+print('hello world'.title())
+
+# title() function used to convert the first character in each word to Uppercase :) .
